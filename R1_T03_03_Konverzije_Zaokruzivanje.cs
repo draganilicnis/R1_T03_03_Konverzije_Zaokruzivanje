56ac5823cb9072c87402a6309f21a80ae488b5f7
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2025-10-03 12:15:36 +0200

    Create R1_T03_03_Konverzije_Zaokruzivanje.cs

---
+++ R1_T03_03_Konverzije_Zaokruzivanje.cs
@@ -0,0 +1,55 @@
+// R1 T03: Promenljive, podaci, tipovi
+// R2 T03.03: Konverzije numerickih tipova i zaokrugljivanje
+// https://petlja.org/sr-Latn-RS/kurs/17862/6/ 
+// https://www.h-schmidt.net/FloatConverter/IEEE754.html
+
+using System;
+
+class R1_T03_03_Konverzije_Zaokruzivanje
+{
+    static void Main()
+    {
+        Tip_Realan_broj();
+    }
+
+    static void Tip_Realan_broj()
+    {
+        Console.WriteLine("Realni brojevi: double, float, decimal");
+        Tip_Realan_broj_double();
+    }
+
+    static void Tip_Realan_broj_double()
+    {
+        Console.WriteLine("double");
+        double a = double.MaxValue;
+        Console.WriteLine(a);
+        Tip_Realan_broj_double_Preciznost();
+        double b = 1e16;    // 1e15
+        Tip_Realan_broj_double_Preciznost(b);
+    }
+    static void Tip_Realan_broj_double_Preciznost()
+    {
+        Console.WriteLine("Tip_Realan_broj_double_Preciznost:");
+        Console.WriteLine("1e0 + 1 - 1e0");
+        Console.WriteLine(1e0 + 1 - 1e0);
+        Console.WriteLine(1e7 + 1 - 1e7);
+        Console.WriteLine(1e8 + 1 - 1e8);
+        Console.WriteLine(1e10 + 1 - 1e10);
+        Console.WriteLine(1e16 + 1 - 1e16);
+    }
+    static void Tip_Realan_broj_double_Preciznost(double a)
+    {
+        Console.WriteLine("Tip_Realan_broj_double_Preciznost(1e16 + 1):");
+        double b = a + 1;
+        double r = a - b;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(r);
+        a = a / 10;
+        b = a + 1;
+        r = a - b;
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(r);
+    }
+}
