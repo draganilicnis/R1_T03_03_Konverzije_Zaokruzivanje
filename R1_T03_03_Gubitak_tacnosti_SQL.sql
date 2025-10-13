6bcb9eb5ab6a96f981b9b88c26ef7ec709f36147
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2025-10-13 15:03:07 +0200

    Create R1_T03_03_Gubitak_tacnosti_SQL.sql

---
+++ R1_T03_03_Gubitak_tacnosti_SQL.sql
@@ -0,0 +1,8 @@
+-- SQL   https://onecompiler.com/sqlserver/43zhdwm4g
+SELECT 0.1 + 0.2          AS rez1;	-- .3
+SELECT 0.3 / 0.1          AS rez2;	-- 3.000000
+SELECT 0.1 + 0.2 - 0.3    AS rez3;	-- .0
+SELECT 3 - 0.3 / 0.1      AS rez4;	-- .000000
+SELECT 9007199254740993.0 AS rez5;	-- 9007199254740993.0
+SELECT 9007199254740993.0 - 9007199254740992.0 AS rez6;	-- 1.0
+SELECT 1.0 / 10 + 2.0 / 10 - 3.0 / 10          AS rez7;	-- .000000
